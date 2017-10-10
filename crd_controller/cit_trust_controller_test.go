008769c5a9678cacf3fd8dc44b18b2bc389b94c8
root <ganeshx.navgire@intel.com>
2017-10-10 14:27:27 +0530

    updated unit test for cit custom controller

--- crd_controller/cit_trust_controller_test.go
+++ crd_controller/cit_trust_controller_test.go
@@ -1,22 +1,47 @@
 package crd_controller
 
 import (
-	//apiextcs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
-	//"k8s.io/client-go/kubernetes/fake"
-	"apiextensions-apiserver/test/integration/testserver"
-	//"meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/rest"
-	 trust_schema "citcrd/crd_schema/cit_trust_schema"
-	 "testing"
+	trust_schema "cit_custom_controller/crd_schema/cit_trust_schema"
+	"testing"
 	)
 
-func TestTLCRDCreation(t *testing.T) {
-	masterConfig, err := rest.InClusterConfig()
-	fakeClient := &fake.Clientset{}
-	err := NewcitTLCustomResourceDefinition(fakeClient)
-        if err != nil {
-                t.Fatalf("error creating Trust Label CRD: %v", err)
-        }
+func TestGetTLCrdDef(t *testing.T) {
+	expecTlCrd := CrdDefinition{
+		Plural:   "trustcrds",
+                Singular: "trustcrd",
+                Group:    "cit.intel.com",
+                Kind:     "TrustCrd",
+	}
+	recvTlCrd := GetTLCrdDef()
+	if expecTlCrd != recvTlCrd {
+		t.Fatalf("Changes found in TL CRD Definition ")
+		t.Fatalf("Expected :%v however Received: %v ", expecTlCrd, recvTlCrd)
+	}
+	t.Logf("Test GetTLCrd Def success")
+}
 
-        t.Logf("Testing cit TL controller success")
+func TestGetTlObjLabel(t *testing.T) {
+	trust_obj := trust_schema.HostList {
+		Hostname : "Node123",
+		Trusted : "true",
+		TrustTagExpiry : "12-23-45T123.91.12",
+		TrustTagSignedReport : "495270d6242e2c67e24e22bad49dgdah",
+	}
+	recvlabel, recannotate := GetTlObjLabel(trust_obj)
+	if _, ok := recvlabel["trusted"]; ok {
+		t.Logf("Found in TL label Trusted field")
+	}else {
+		t.Fatalf("Could not get label trusted from TL Report")
+	} 
+	if _, ok := recvlabel["TrustTagExpiry"]; ok {
+		t.Logf("Found in TL label TrustTagExpiry field")
+	}else {
+		t.Fatalf("Could not get label TrustTagExpiry from TL Report")
+	} 
+	if _, ok := recannotate["TrustTagSignedReport"]; ok {
+		t.Logf("Found in TL annotation TrustTagSignedReport ")
+	}else {
+		t.Fatalf("Could not get annotation TrustTagSignedReport from TL Report")
+	} 
+	t.Logf("Test getTlObjLabel success")
 }
